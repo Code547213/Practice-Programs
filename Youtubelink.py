900cc0ee87c07e1c2ba8c688315aee58e5275898
A.Antony Rajesh <104267267+Code547213@users.noreply.github.com>
2022-09-05 20:22:46 +0530

    Add files via upload

---
+++ Youtubelink.py
@@ -0,0 +1,28 @@
+from pytube import YouTube
+import os
+from pathlib import Path
+
+
+link=input("enter the youtube link: ")
+choice=input("Enter 'a' for audio / 'v' for video:")
+while(choice!="a" and choice!="v"):
+    print("Enter correct choice!")
+    choice=input("Enter 'a' for audio / 'v' for video:")
+
+
+if choice=="a":
+    audio=YouTube(link)
+    print("Please Wait for minutes.......")
+    downloads_path = str(Path.home() / "Downloads")
+    # download the video and store it in a folder called "YoutubeAudios" inside the downloads folder
+    audio.streams.get_audio_only().download(output_path=os.path.join(downloads_path, 'YoutubeAudios'))
+    print("Open Downloads and Under YoutubeAudios, You will find the audio")
+    
+
+
+if choice=="v":
+    video=YouTube(link)
+    downloads_path = str(Path.home() / "Downloads")
+    print("Please Wait for minutes.......")
+    video.streams.get_by_resolution("720p").download(output_path=os.path.join(downloads_path, 'YoutubeVideos'))
+    print("Open Downloads and Under YoutubeVideos, You will find the video")
